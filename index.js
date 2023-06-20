9242add8a072d1501adc9fd05828f1ee96cde980
rxdn <29165304+rxdn@users.noreply.github.com>
2023-06-20 12:31:34 +0100

    Remove default config

--- index.js
+++ index.js
@@ -10,66 +10,34 @@ import * as fs from 'fs';
         //If the path  is not set, return the Buffer of image.
         path:  '', // Path is filepath of the image which will be created.
     }
-
  *
  */
 export default function(config) {
-    var ctx = createCanvas(128, 128);
+    const ctx = createCanvas(128, 128);
     if (config.font) {
         ctx.font = config.font;
     }
 
     echarts.setPlatformAPI({ createCanvas });
 
-    var chart, option = {
-        title: {
-            text: 'test'
-        },
-        tooltip: {},
-        legend: {
-            data: ['test']
-        },
-        xAxis: {
-            data: ["a", "b", "c", "d", "f", "g"]
-        },
-        yAxis: {},
-        series: [{
-            name: 'test',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    };
-
-    let defaultConfig = {
-      width: 500,
-      height: 500,
-      option,
-      enableAutoDispose: true
-    }
-
-    config = Object.assign({}, defaultConfig, config)
-
     config.option.animation = false;
-    chart = echarts.init(createCanvas(parseInt(config.width, 10), parseInt(config.height, 10)));
+
+    const chart = echarts.init(createCanvas(parseInt(config.width, 10), parseInt(config.height, 10)));
     chart.setOption(config.option);
     if (config.path) {
         try {
             fs.writeFileSync(config.path, chart.getDom().toBuffer());
-            if(config.enableAutoDispose){
-              chart.dispose();
-            }
+            chart.dispose();
             console.log("Create Img:" + config.path)
         } catch (err) {
             console.error("Error: Write File failed" + err.message)
-        }
-        
+        }  
     } else {
-        var buffer = chart.getDom().toBuffer();
-        try{
-          if(config.enableAutoDispose){
+        const buffer = chart.getDom().toBuffer();
+        try {
             chart.dispose();
-          }
-        }catch(e){}
+        } catch(e) {}
+        
         return buffer;
     }
 }
